3ef8be0fd7a002046a65e2fea8f19fb410511a3d
william baierle charqueiro <nittts@pop-os.localdomain>
2022-12-10 05:14:53 -0300

    fix: vercel deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
 import AppRouter from "./Routes/index.routes";
 import { AppError } from "./errors";
+import path from "path";
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/v1", AppRouter);
 
@@ -14,6 +16,10 @@ app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
       message: err.message,
     });
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
 export default app;
