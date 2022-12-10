6603655fc9ee60009b5816a282c7c16651389fc1
william baierle charqueiro <nittts@pop-os.localdomain>
2022-12-10 00:19:08 -0300

    chore: changed console.log api url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,5 +4,5 @@ import app from "./app";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("App running at http://localhost:" + port);
+  console.log("App running at http://localhost:" + port + "/api/v1");
 });
