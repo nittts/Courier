4135c551c5931913fbe90713994509fd3393e93b
william baierle charqueiro <nittts@pop-os.localdomain>
2022-12-13 23:47:21 -0300

    feat: included cors for app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,9 @@
-import path from "path";
 import AppRouter from "./Routes/index.routes";
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 
 const app = express();
 app.use(express.json());
-app.use(express.static("public"));
 
 app.use("/api/v1", AppRouter);
-
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "public") });
-});
 
 export default app;
